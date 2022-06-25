550dc9b41eb940871ebcea472e95da1ada5c1f7b
t-94 <infourbh@gmail.com>
2022-06-25 19:52:45 +0900

    【やってみた】アニメーション追加してみた【草】

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useState } from 'react'
 import styles from "../styles/pages/index.module.scss"
 
 const Index: NextPage = () => {
+  const [timestamp, setTimestamp] = useState("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTimestamp(e.target.value)
+  }
+
   return (
     <div className={styles.wrap}>
       <Head>
@@ -13,8 +20,8 @@ const Index: NextPage = () => {
         <span className={styles.boxTop}></span>
         <span className={styles.boxBottom}></span>
         <div className={styles.main}>
-          <input className={styles.input} type="text" />
-          <div className={styles.timestamp}>うんこ</div>
+          <input onChange={handleChange} className={styles.input} type="text" />
+          <div className={`${styles.timestamp} ${timestamp.length ? styles.fadeInUp : ""}`}>{timestamp}</div>
         </div>
       </div>
     </div>
