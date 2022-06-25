9457eee69b2fb3a7fa874eb8bafe2b22b6ff85f4
t-94 <infourbh@gmail.com>
2022-06-25 18:41:03 +0900

    404ページ作った

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Custom404: NextPage = () => {
+  return (
+    <>
+    <Head><title>NOT FOUND</title></Head>
+    <h1>NOT FOUND</h1>
+    </>
+  )
+}
+
+export default Custom404
