e66d25c114561301470f037519f2c51eac1fee2a
t-94 <infourbh@gmail.com>
2022-06-25 19:21:45 +0900

    ざっくりレイアウト組んだった

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import styles from "../styles/pages/index.module.scss"
 
-const Home: NextPage = () => {
+const Index: NextPage = () => {
   return (
-    <>
-    <Head><title>ULID Timestamp</title></Head>
-    <h1>ULID Timestamp</h1>
-    </>
+    <div className={styles.wrap}>
+      <Head>
+        <title>ULID Timestamp</title>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Get The Timestamp From A Given ULID</h1>
+        <span className={styles.boxTop}></span>
+        <span className={styles.boxBottom}></span>
+      </div>
+    </div>
   )
 }
 
-export default Home
+export default Index
